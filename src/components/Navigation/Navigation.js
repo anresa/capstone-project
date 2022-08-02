4c454965ee9662bf4c8c4546c6cee2e13795442e
Anna Sandoval <art_90@web.de>
2022-08-02 15:38:06 +0200

    add navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,5 +1,57 @@
 import './Navigation.css';
+import {IoIosAddCircleOutline} from 'react-icons/io';
+import {IoCopyOutline} from 'react-icons/io5';
+import {IoIosMenu} from 'react-icons/io';
+import {IoIosHeartEmpty} from 'react-icons/io';
+import {IoPersonOutline} from 'react-icons/io5';
+
+const list = document.querySelectorAll('.list');
+function activeLink() {
+  list.forEach(item => item.classList.remove('active'));
+  this.classList.add('active');
+}
+list.forEach(item => item.addEventListener('click', activeLink));
 
 export default function Navigation() {
-  return <h2>Navigation</h2>;
+  return (
+    <>
+      <div className="navigation">
+        <section className="navigation-box">
+          <ul>
+            <li className="list active">
+              <a href="/add">
+                <IoIosAddCircleOutline className="icon" />
+                <span className="text">Add</span>
+              </a>
+            </li>
+            <li className="list">
+              <a href="/categories">
+                <IoCopyOutline className="icon" />
+                <span className="text">Categories</span>
+              </a>
+            </li>
+            <li className="list">
+              <a href="/">
+                <IoIosMenu className="icon" />
+                <span className="text">Home</span>
+              </a>
+            </li>
+            <li className="list">
+              <a href="/favorites">
+                <IoIosHeartEmpty className="icon" />
+                <span className="text">Favorites</span>
+              </a>
+            </li>
+            <li className="list">
+              <a href="/profile">
+                <IoPersonOutline className="icon" />
+                <span className="text">Profile</span>
+              </a>
+            </li>
+            <div className="indicator"></div>
+          </ul>
+        </section>
+      </div>
+    </>
+  );
 }
